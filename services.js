57c00e950efb7c0ee088cdefd0ad812cc7719c7e
isaacmaddox <54295143+isaacodes@users.noreply.github.com>
2020-03-23 21:41:31 -0700

    snap scrolling (dor dad)

--- services.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $("body").scroll(function() {
-        if ($("nav").offset().top == 0) {
-            $("nav").addClass("top");
-        } else {
-            $("nav").removeClass("top");
-        }
-    });
-});
