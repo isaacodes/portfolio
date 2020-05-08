8b20d05fd274f696d070aa890a6cd7e047b3892b
isaacmaddox <54295143+isaacodes@users.noreply.github.com>
2020-05-07 21:49:01 -0700

    update!
    Not yet compatible with mobile, working on that!

--- scripts.js
+++ scripts.js
@@ -1,109 +1,73 @@
-$(document).ready(function() {
-    var currentslide = 0;
-    var able = true;
-    window.addEventListener('wheel', function(event)
-        {
-        if (able === true) {
-            var bh = $("body").height();
-            if (event.deltaY < 0) {
-                switch (currentslide - 1) {
-                    case 0:
-                        $("body, html").animate({
-                            scrollTop: '0',
-                        }, 300);
-                        $("nav").removeClass('trigger');
-                        break;
-                    case 1:
-                        $("body, html").animate({
-                            scrollTop: bh,
-                        }, 300);
-                        break;
-                    case 2:
-                        $("body, html").animate({
-                            scrollTop: bh * 2,
-                        }, 300);
-                        break;
-                    case 3:
-                        $("body, html").animate({
-                            scrollTop: bh * 3,
-                        }, 300);
-                        break;
-                    case 4:
-                        $("body, html").animate({
-                            scrollTop: bh * 4,
-                        }, 300);
-                        break;
-                    case 5:
-                        $("body, html").animate({
-                            scrollTop: bh * 5,
-                        }, 300);
-                        break;
-                }
-                currentslide--;
-                if (currentslide < 0) {
-                    currentslide = 0;
-                }
-            } else if (event.deltaY > 0) {
-                switch (currentslide + 1) {
-                    case 1:
-                        $("body, html").animate({
-                            scrollTop: bh,
-                        }, 300);
-                        $("nav").addClass("trigger");
-                        break;
-                    case 2:
-                        $("body, html").animate({
-                            scrollTop: bh * 2,
-                        }, 300);
-                        break;
-                    case 3:
-                        $("body, html").animate({
-                            scrollTop: bh * 3,
-                        }, 300);
-                        break;
-                    case 4:
-                        $("body, html").animate({
-                            scrollTop: bh * 4,
-                        }, 300);
-                        break;
-                    case 5:
-                        $("body, html").animate({
-                            scrollTop: bh * 5,
-                        }, 300);
-                        break;
-                    case 6:
-                        $("body, html").animate({
-                            scrollTop: bh * 6,
-                        }, 300);
-                        break;
-                }
-                currentslide++;
-                if (currentslide > 6) {
-                    currentslide = 6;
-                }
-            }
-            able = false;
-            function puedo() {
-                able = true;
-            }
-            setTimeout(puedo, 400);
-        }
-        checkslide();
-    });
-    function checkslide() {
-        $(".section").each(function() {
-            var y = parseInt($(this).prop('id'));
-            if (y == currentslide) {
-                display(y);
-            } else {
-                $(this).removeClass('display');
-            }
-        });
+$(document).scroll(function() {
+    var st = $(document).scrollTop();
+    var bh = $("header").height();
+    if (st >= bh) {
+        $("nav").addClass('top');
+    } else {
+        $("nav").removeClass('top');
     }
-    function display(slidenumber) {
-        function wait() {
-            $(".section#" + slidenumber).addClass('display');
+    $("section").each(function() {
+        var top = $(this).offset().top;
+        if (st + 65 >= top - (bh / 3)) {
+            $(this).addClass('show');
+            if ($(this).prop('id') == "three") {
+                progressbar();
+            }
+        } else {
+            // $(this).removeClass('show');
         }
-        setTimeout(wait, 500);
+    });
+    $("body").css('background-position-y', 'calc(' + -st / 3 + 'px)');
+}).on('click', '.project', function() {
+    var id = $(this).prop('id');
+    switch (id) {
+        case "sd":
+            window.open('https://simplisticdesign.netlify.app');
+            break;
+        case "rwd":
+            window.open('http://isaacodes.github.io/ruckelwebdesign');
+            break;
+        case "dbf":
+            window.open('http://isaacodes.github.io/destinbayfishing');
+            break;
+    }
+}).ready(function() {
+    $("section").each(function() {
+        var top = $(this).children('h2').offset().top + $(this).children('h2').height() - $(this).offset().top;
+        $('.underline').css('top', 'calc(' + top + 'px + 2vh)');
+    });
+});
+function progressbar() {
+    $(".inner-bar").each(function() {
+        var width = $(this).html();
+        $(this).css('width', 'calc(' + width + ' - 16px)').css('padding-right', '16px');
+    });
+}
+
+// Resize Handling
+$(window).resize(function() {
+    if ($("body").width() <= 1086) {
+        $("nav").addClass('collapse');
+    } else {
+        $("nav").removeClass('collapse');
+    }
+
+    if ($("body").width() <= 900) {
+        $("nav").addClass('mobile');
+    } else {
+        $("nav").removeClass('mobile');
+    }
+});
+
+var navopen = false;
+$(document).on('click', 'nav button.expand-nav', function() {
+    if (navopen === false) {
+        $(this).addClass('nav-open');
+        $("nav").addClass('open');
+        navopen = true;
+    } else {
+        $(this).removeClass('nav-open');
+        $("nav").removeClass("open");
+        navopen = false;
     }
 });
